685c23941b06f3aceb27df6538dd21a7a51b268d
TestError0 <ashraf.inflack@gmail.com>
2024-04-30 10:16:26 +0600

    First Day Compleated code

---
+++ HabPro/Basic-HabPro/app.js
@@ -0,0 +1,84 @@
+// JavaScript Variables
+
+//?LET  variales
+
+{
+    let b =10;
+
+console.log(b);
+
+}
+
+
+
+
+
+
+
+
+
+/*
+?var variable
+*Can re declare
+*can re assign
+*Global
+*/
+
+
+/*
+var a = 10;
+var a =20;
+a ='Dhaka';
+{
+    var blocked = '30';
+    console.log(a);
+}
+console.log(blocked);
+*/
+
+
+
+
+
+
+/*
+let fruits = 'Mango';
+console.log(`${fruits} is my fav`);
+*/
+
+
+
+
+//?Comment Section
+
+/*
+**Multiline comment
+? some info
+TODO: something I need
+function funcName(){
+    console.log('My First Function');
+
+}
+funcName();
+
+*/
+
+
+
+
+
+//Starting code
+
+/*
+console.log('Hello JavaScript');
+
+let sum = 50 + 60;
+console.log(sum)
+
+let firstname = 'Kazi';
+let age = '20';
+
+console.log(firstname);
+console.log(age);
+
+*/
