563c070008ff521494934dfa42655cb460db7a7b
TestError0 <ashraf.inflack@gmail.com>
2024-05-12 12:00:03 +0600

    Logical Operater added

--- HabPro/Basic-HabPro/app.js
+++ HabPro/Basic-HabPro/app.js
@@ -1,17 +1,147 @@
-// JavaScript Variables
+
+
+
+
+
+
+
+
+/*
+
+//Logical Operater
+
+// AND(&&) = 2ta e true hote hobe
+// OR(||) = 2ta false holei false otherwise sob true
+// NOT (!) = not use kore not value banano
+
+
+*/
+
+
+
+
+
+ 
+/*
+//Comparison Operator
+
+let p = '5';
+let q = 4;
+
+console.log(p == q); // equal to 
+console.log(p === q); // equal value and equal type
+console.log(p != q); //not equal
+console.log(p > q);  // Grater Then
+console.log(p < q);  //Less then 
+console.log(p > q);  //Less then Equal
+console.log(p >= q);  //Less then or Equal to
+console.log(p <= q); //Greater Then Equal to
+
+*/
+
+
+
+
+
+
+//Increment & Decrement 
+
+/*
+
+let x = 5;
+x = x + 1;
+x++;
+console.log(`Increment ${x}`);
+
+
+let y = 5;
+y = y - 1;
+y--; 
+console.log(`Decrement ${y}`);
+
+*/
+
+
+
+
+
+
+/*
+
+//Arithmetic Operators
+
+let userInput = prompt('Enter a number');
+let number = parseInt(userInput);
+
+if(number %2 === 0){
+    console.log(`${number} is Even Number`);
+}
+else{
+    console.log(`${number} is Odd Number`);
+}
+
+
+// if(number % 2 ===0){
+//     console.log`This is Odd Number`;
+// }
+
+// else{
+//     console.log`This is Even Number`;
+// }
+
+
+
+
+// let a = 10;
+// let b = 5;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+*/
+
+
+
+
+
+
+
+/*
+// ?JavaScript Variables
+
+
+const c = 10;
+const e = 20;
+
+{
+    const d =50;
+    console.log(c);
+    console.log(e);
+}
+
+*/
+
+
+
+
+/*
 
 //?LET  variales
 
+let b =10;
+b = 11;
+
 {
-    let b =10;
 
 console.log(b);
 
 }
 
 
-
-
+*/
 
 
 
